09dc782a817f06f21e83d17bf94071612823d5b2
it-houxy <91185477+it-houxy@users.noreply.github.com>
2023-11-08 11:00:31 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@
 SECRET_KEY = "asghnjadfbtywyn"
 
 # openAi api key
-OPENAI_API_KEY = ""
+OPENAI_API_KEY = "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"
 
 # openAi 官方 api
 URL = "https://api.openai.com/v1/chat/completions"
